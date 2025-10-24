c6dc334cc6e0c576c78e3cfd86ab4ffe8bd2f5c5
Ishalana <laneech2@gmail.com>
2025-10-24 10:54:29 -0500

    Renamed CombineMeLater to Program

--- ProjectTwo/obj/Debug/net9.0/ProjectTwo.AssemblyInfo.cs
+++ ProjectTwo/obj/Debug/net9.0/ProjectTwo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectTwo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cab02a627244dd22a1e7e966c93522c07c47da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0aa513603414912f1a81c7da5ceabb037d361df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectTwo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectTwo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
