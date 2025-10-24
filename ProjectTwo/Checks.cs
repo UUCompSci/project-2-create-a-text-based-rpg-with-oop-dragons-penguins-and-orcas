2af72551e6b7c89b7c175e13d674e60621293dcc
Ishalana <laneech2@gmail.com>
2025-10-23 19:53:50 -0500

    Added some comments

--- ProjectTwo/Checks.cs
+++ ProjectTwo/Checks.cs
@@ -1,3 +1,7 @@
+// Author; Elena Hazard
+// Date: 10/23/2025
+
+// Class that of methods, the 1st checks if a key is pushed, the 2nd will give an error message, and the 3rd checks if you have won the game.
 static class Checks
 {
     public static ConsoleKey GetKey()
@@ -24,15 +28,16 @@ static class Checks
     }
 }
 
+// Class that holds const varibles of ConsoleKey to make choices with.
 public static class CheckKey
-    {
-        public const ConsoleKey _1 = ConsoleKey.D1;
-        public const ConsoleKey _2 = ConsoleKey.D2;
-        public const ConsoleKey _3 = ConsoleKey.D3;
-        public const ConsoleKey _4 = ConsoleKey.D4;
-        public const ConsoleKey _5 = ConsoleKey.D5;
-        public const ConsoleKey _6 = ConsoleKey.D6;
-        public const ConsoleKey _7 = ConsoleKey.D7;
-        public const ConsoleKey _8 = ConsoleKey.D8;
-        public const ConsoleKey _9 = ConsoleKey.D9;
-    }
+{
+    public const ConsoleKey _1 = ConsoleKey.D1;
+    public const ConsoleKey _2 = ConsoleKey.D2;
+    public const ConsoleKey _3 = ConsoleKey.D3;
+    public const ConsoleKey _4 = ConsoleKey.D4;
+    public const ConsoleKey _5 = ConsoleKey.D5;
+    public const ConsoleKey _6 = ConsoleKey.D6;
+    public const ConsoleKey _7 = ConsoleKey.D7;
+    public const ConsoleKey _8 = ConsoleKey.D8;
+    public const ConsoleKey _9 = ConsoleKey.D9;
+}
